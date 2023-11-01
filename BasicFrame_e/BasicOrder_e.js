8d7c277c88ee42c8c7de5eec8b51b19e7de46cd3
Ji Eunyoung <jeunyeong323@gmail.com>
2023-11-01 20:07:19 +0900

    메시지 수정

--- BasicFrame_e/BasicOrder_e.js
+++ BasicFrame_e/BasicOrder_e.js
@@ -264,7 +264,7 @@ function firstScreen() {
       const linkElement = document.createElement("link");
       linkElement.rel = "stylesheet";
       linkElement.type = "text/css";
-      linkElement.href = "http://localhost:3001/payment_msg/payment_msg_e.css"; // 이 부분의 파일 경로를 수정해야합니다.
+      linkElement.href = "http://localhost:3001/payment_msg_e/payment_msg_e.css"; // 이 부분의 파일 경로를 수정해야합니다.
       document.head.appendChild(linkElement);
 
       // 모달을 열기 위한 코드
@@ -1074,7 +1074,7 @@ function updateData() {
           const linkElement = document.createElement("link");
           linkElement.rel = "stylesheet";
           linkElement.type = "text/css";
-          linkElement.href = "http://localhost:3001/payment_msg_e/payment_msg_e.css";
+          linkElement.href = "http://localhost:3001/help_msg/help_msg.css";
           document.head.appendChild(linkElement);
 
           const modalTitle = document.querySelector(".modal-title");
